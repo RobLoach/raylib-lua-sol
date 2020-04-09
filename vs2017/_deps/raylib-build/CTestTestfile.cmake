1ee7a6bd8c5777b6d3284bc84cd3b59f965a0252
xharris <xhh950@gmail.com>
2020-04-08 22:08:13 -0400

    some vs config stuff

--- vs2017/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-src
-# Build directory: D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- vs2017/_deps/raylib-build/src/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-src/src
-# Build directory: D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pkg-config--static "D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-src/src/../cmake/test-pkgconfig.sh" "--static")
-set_tests_properties(pkg-config--static PROPERTIES  _BACKTRACE_TRIPLES "D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-src/src/CMakeLists.txt;180;add_test;D:/Documents/PROJECTS/raylib-lua-sol/build/_deps/raylib-src/src/CMakeLists.txt;0;")
-subdirs("external/glfw")
